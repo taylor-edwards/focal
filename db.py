fad4895791688c8a99a49de6ede2918680d89de7
Taylor Edwards <me@tayloredwards.net>
2021-10-28 17:11:01 -0700

    Switch primary DB interface to use SQLAlchemy's ORM

--- db.py
+++ db.py
@@ -1,7 +1,46 @@
 from datetime import datetime
-from sqlalchemy import \
-    Column, create_engine, Date, ForeignKey, Identity, \
-    Integer, inspect, MetaData, String, Table
+from sqlalchemy import Column, create_engine, Date, ForeignKey, \
+    Identity, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+base_class = declarative_base()
+
+class User(base_class):
+    __tablename__ = 'user'
+    user_id = Column('user_id', Integer, Identity(start=100, cycle=True), primary_key=True)
+    user_name = Column('user_name', String(24), unique=True, nullable=False)
+    date_created = Column('date_created', Date, nullable=False)
+
+class Album(base_class):
+    __tablename__ = 'album'
+    album_id = Column('album_id', Integer, Identity(start=200, cycle=True), primary_key=True)
+    album_name = Column('album_name', String)
+    user_id = Column('user_id', Integer, ForeignKey('user.user_id', ondelete='CASCADE', onupdate='CASCADE'))
+    date_created = Column('date_created', Date, nullable=False)
+    date_edited = Column('date_edited', Date, nullable=False)
+
+class Photo(base_class):
+    __tablename__ = 'photo'
+    photo_id = Column('photo_id', Integer, Identity(start=300, cycle=True), primary_key=True)
+    photo_name = Column('photo_name', String)
+    user_id = Column('user_id', Integer, ForeignKey('user.user_id', ondelete='CASCADE', onupdate='CASCADE'))
+    album_id = Column('album_id', Integer, ForeignKey('album.album_id', ondelete='CASCADE', onupdate='CASCADE'))
+    date_created = Column('date_created', Date, nullable=False)
+    date_edited = Column('date_edited', Date, nullable=False)
+    file_preview = Column('file_preview', String, nullable=False)
+    file_source = Column('file_source', String, unique=True, nullable=False)
+
+class Edit(base_class):
+    __tablename__ = 'edit'
+    edit_id = Column('edit_id', Integer, Identity(start=400, cycle=True), primary_key=True)
+    edit_name = Column('edit_name', String)
+    user_id = Column('user_id', Integer, ForeignKey('user.user_id', ondelete='CASCADE', onupdate='CASCADE'))
+    photo_id = Column('photo_id', Integer, ForeignKey('photo.photo_id', ondelete='CASCADE', onupdate='CASCADE'))
+    date_created = Column('date_created', Date, nullable=False)
+    date_edited = Column('date_edited', Date, nullable=False)
+    file_preview = Column('file_preview', String, nullable=False)
+    file_source = Column('file_source', String, unique=True, nullable=False)
 
 def get_time():
     return datetime.now().isoformat()
@@ -9,119 +48,151 @@ def get_time():
 class Database:
     def __init__(self, connection_string):
         self.db = create_engine(connection_string)
-        self.meta = MetaData(self.db)
-        self.__create_users__()
-        self.__create_albums__()
-        self.__create_photos__()
-        self.__create_edits__()
+        self.__session__ = None
+        base_class.metadata.create_all(self.db)
 
-    def __create_users__(self):
-        user_id = Column('user_id', Integer, Identity(start=100, cycle=True), primary_key=True)
-        user_name = Column('user_name', String, unique=True, nullable=False)
-        date_created = Column('date_created', Date, nullable=False)
-        self.users = Table(
-            'users',
-            self.meta,
-            user_id,
-            user_name,
-            date_created
-        )
+    def create_session(self):
+        if self.__session__ == None:
+            self.create_session = sessionmaker(self.db)
+            self.__session__ = self.create_session()
+        return self.__session__
 
-    def __create_albums__(self):
-        album_id = Column('album_id', Integer, Identity(start=200, cycle=True), primary_key=True)
-        album_name = Column('album_name', String)
-        user_id = Column('user_id', Integer, ForeignKey('users.user_id'))
-        date_created = Column('date_created', Date, nullable=False)
-        date_edited = Column('date_edited', Date, nullable=False)
-        self.albums = Table(
-            'albums',
-            self.meta,
-            album_id,
-            album_name,
-            user_id,
-            date_created,
-            date_edited
-        )
-
-    def __create_photos__(self):
-        photo_id = Column('photo_id', Integer, Identity(start=300, cycle=True), primary_key=True)
-        photo_name = Column('photo_name', String)
-        user_id = Column('user_id', Integer, ForeignKey('users.user_id'))
-        album_id = Column('album_id', Integer, ForeignKey('albums.album_id'))
-        date_created = Column('date_created', Date, nullable=False)
-        date_edited = Column('date_edited', Date, nullable=False)
-        file_preview = Column('file_preview', String, nullable=False)
-        file_source = Column('file_source', String, unique=True, nullable=False)
-        self.photos = Table(
-            'photos',
-            self.meta,
-            photo_id,
-            photo_name,
-            user_id,
-            album_id,
-            date_created,
-            date_edited,
-            file_preview,
-            file_source
-        )
-
-    def __create_edits__(self):
-        edit_id = Column('edit_id', Integer, Identity(start=400, cycle=True), primary_key=True)
-        edit_name = Column('edit_name', String)
-        user_id = Column('user_id', Integer, ForeignKey('users.user_id'))
-        photo_id = Column('photo_id', Integer, ForeignKey('photos.photo_id'))
-        date_created = Column('date_created', Date, nullable=False)
-        date_edited = Column('date_edited', Date, nullable=False)
-        file_preview = Column('file_preview', String, nullable=False)
-        file_source = Column('file_source', String, unique=True, nullable=False)
-        self.edits = Table(
-            'edits',
-            self.meta,
-            edit_id,
-            edit_name,
-            user_id,
-            photo_id,
-            date_created,
-            date_edited,
-            file_preview,
-            file_source
-        )
-
-    def bootstrap(self):
-        i = inspect(self.db)
-        has_users = i.has_table('users')
-        has_albums = i.has_table('albums')
-        has_photos = i.has_table('photos')
-        has_edits = i.has_table('edits')
-        if not (has_users and has_albums and has_photos and has_edits):
-            with self.db.connect() as conn:
-                if not has_users:
-                    self.users.create()
-                if not has_albums:
-                    self.albums.create()
-                if not has_photos():
-                    self.photos.create()
-                if not has_edits():
-                    self.edits.create()
+    def close_session(self):
+        if self.__session__ != None:
+            self.__session__.close()
+        self.__session__ = None
 
     def select_all_users(self):
-        with self.db.connect() as conn:
-            return conn.execute('SELECT user_name FROM users').mappings().all()
+        with self.create_session() as session:
+            return session.query(User).all()
+
+    def select_user(self, user_id):
+        with self.create_session() as session:
+            return session.query(User).filter_by(user_id=user_id).first()
+
+    def select_users_by_name(self, user_name_list):
+        with self.create_session() as session:
+            return session.query(User.user_id).filter(User.user_name.in_(user_name_list)).all()
 
     def insert_users(self, user_name_list):
-        with self.db.connect() as conn:
+        with self.create_session() as session:
             time = get_time()
             for user_name in user_name_list:
-                statement = self.users.insert().values(
-                    user_name=user_name,
-                    date_created=time
-                )
-                conn.execute(statement)
+                user = session.query(User).filter_by(user_name=user_name).first()
+                if user == None:
+                    session.add(User(user_name=user_name, date_created=time))
+            session.commit()
+            return self.select_users_by_name(user_name_list)
 
-    def delete_users(self, user_name_list):
-        with self.db.connect() as conn:
-            for user_name in user_name_list:
-                statement = self.users.delete().where(self.users.c.user_name == user_name)
-                conn.execute(statement)
+    def delete_users(self, user_id_list):
+        with self.create_session() as session:
+            deleted_users = []
+            for user_id in user_id_list:
+                user = self.select_user(user_id)
+                if user != None:
+                    session.delete(user)
+                    deleted_users.append(user_id)
+            session.commit()
+            return deleted_users
 
+    def select_album(self, album_id):
+        with self.create_session() as session:
+            return session.query(Album).filter_by(album_id=album_id).first()
 
+    def select_user_albums(self, user_id):
+        with self.create_session() as session:
+            return session.query(Album).filter_by(user_id=user_id).all()
+
+    def insert_albums(self, album_name_list, user_id):
+        with self.create_session() as session:
+            time = get_time()
+            for album_name in album_name_list:
+                album = session.query(Album.album_name).filter_by(user_id=user_id, album_name=album_name).first()
+                if album == None:
+                    session.add(Album(
+                        album_name=album_name,
+                        user_id=user_id,
+                        date_created=time,
+                        date_edited=time
+                    ))
+            session.commit()
+            return (
+                session
+                .query(Album.album_id)
+                .filter_by(user_id=user_id)
+                .filter(Album.album_name.in_(album_name_list))
+                .all()
+            )
+
+    def delete_albums(self, album_id_list):
+        with self.create_session() as session:
+            deleted_albums = []
+            for album_id in album_id_list:
+                album = self.select_album(album_id)
+                if album != None:
+                    session.delete(album)
+                    deleted_albums.append(album_id)
+            session.commit()
+            return deleted_albums
+
+    def select_photo(self, photo_id):
+        with self.create_session() as session:
+            return session.query(Photo).filter_by(photo_id=photo_id).first()
+
+    def select_user_photos(self, user_id):
+        with self.create_session() as session:
+            return session.query(Photo).filter_by(user_id=user_id).all()
+
+    def select_album_photos(self, album_id):
+        with self.create_session() as session:
+            return session.query(Photo).filter_by(album_id=album_id).all()
+
+    def insert_photo(self, photo_name, user_id, album_id, file_preview, file_source):
+        with self.create_session() as session:
+            photo_id = session.query(Photo.photo_id).filter_by(file_source=file_source).first()
+            if photo_id != None:
+                return photo_id
+
+            time = get_time()
+            session.add(Photo(
+                photo_name=photo_name,
+                user_id=user_id,
+                album_id=album_id,
+                date_created=time,
+                date_edited=time,
+                file_preview=file_preview,
+                file_source=file_source
+            ))
+            session.commit()
+            return session.query(Photo.photo_id).filter_by(file_source=file_source).first()
+
+    def select_edit(self, edit_id):
+        with self.create_session() as session:
+            return session.query(Edit).filter_by(edit_id=edit_id).first()
+
+    def select_user_edits(self, user_id):
+        with self.create_session() as session:
+            return session.query(Edit).filter_by(user_id=user_id).all()
+
+    def select_photo_edits(self, photo_id):
+        with self.create_session() as session:
+            return session.query(Edit).filter_by(photo_id=photo_id).all()
+
+    def insert_edit(self, edit_name, user_id, photo_id, file_preview, file_source):
+        with self.create_session() as session:
+            edit_id = session.query(Edit.edit_id).filter_by(file_source=file_source).first()
+            if edit_id != None:
+                return edit_id
+
+            time = get_time()
+            session.add(Edit(
+                edit_name=edit_name,
+                user_id=user_id,
+                photo_id=photo_id,
+                date_created=time,
+                date_edited=time,
+                file_preview=file_preview,
+                file_source=file_source
+            ))
+            session.commit()
+            return session.query(Edit.edit_id).filter_by(file_source=file_source).first()
--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import sys
-from db2 import Database
+from db import Database
 
-db = Database('postgresql+psycopg2://albumator@127.0.0.1:5432/albumator')
-db.bootstrap()
-users = db.select_all_users()
-print(users)
-# db.insert_users(['jojo'])
-# users = db.select_all_users()
-# print(users)
-# db.delete_users(['jojo'])
-# users = db.select_all_users()
-# print(users)
+db = Database('postgresql+psycopg2://albumator:asdf@127.0.0.1:5432/albumator')
+print(f"Added users {db.insert_users(['chumpy', 'skeletor', 'nacho', 'jojo'])}")
+print(f"All users: {[user.user_name for user in db.select_all_users()]}")
+print(f"Added albums {db.insert_albums(['Landscape photos', 'Portraits'], 142)}")
+print(f"Added photo {db.insert_photo('Mt Akina', 142, 223, '/path/to/preview.jpg', '/path/to/some/file.raw')}")
+print(f"Added edit {db.insert_edit('Mt Akina remix', 142, 302, '/path/to/preview2.jpg', '/path/to/some/file2.xml')}")
+print(f"A user's albums: {[album.album_name for album in db.select_user_albums(142)]}")
+print(f"A user's photos: {[photo.photo_name for photo in db.select_user_photos(142)]}")
+print(f"A photo's edits: {[edit.edit_name for edit in db.select_photo_edits(302)]}")
 
 if __name__ == '__main__':
   sys.exit(0)
